fad37aa108c011a49982035777cbd0b9b7054493
mario welch <mario.welch01@gmail.com>
2022-01-20 16:34:41 -0600

    added comments

--- src/main/java/StringManipulations.java
+++ src/main/java/StringManipulations.java
@@ -7,16 +7,24 @@ public class StringManipulations {
         Scanner sc = new Scanner(System.in);
         String userInput;
 
+        //Grants user the ability to input
         System.out.println("Enter Something: ");
         userInput = sc.nextLine();
 
+        //Prints userInput to console
         System.out.println("You entered: " + userInput);
+
+        //Checks if userInput is number or not
         if(StringUtils.isNumeric(userInput)){
             System.out.println(userInput + " is a number.");
         } else{
             System.out.println(userInput + " is not a number.");
         }
+
+        //Swaps case of userInput by using StringUtils.swapCase()
         System.out.println("Flipped case: " + StringUtils.swapCase(userInput));
+
+        //Reverses userInput by using StringUtils.reverse()
         System.out.println("Reversed: " + StringUtils.reverse(userInput));
     }
 }

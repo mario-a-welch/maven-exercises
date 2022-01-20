7bf3b658722c00996d3c004160f5423a1e08a425
mario welch <mario.welch01@gmail.com>
2022-01-20 15:16:57 -0600

    stringutils class exercise

---
+++ src/main/java/StringManipulations.java
@@ -0,0 +1,22 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class StringManipulations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String userInput;
+
+        System.out.println("Enter Something: ");
+        userInput = sc.next();
+
+        System.out.println("You entered: " + userInput);
+        if(StringUtils.isNumeric(userInput)){
+            System.out.println(userInput + " is a number.");
+        } else{
+            System.out.println(userInput + " is not a number.");
+        }
+        System.out.println("Flipped case: " + StringUtils.swapCase(userInput));
+        System.out.println("Reversed: " + StringUtils.reverse(userInput));
+    }
+}
